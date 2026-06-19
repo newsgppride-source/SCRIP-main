6dc0967ab638792163e7b9f74dbf855e9e034135
newsgppride-source <newsgppride@gmail.com>
2026-06-19 19:36:05 +0800

    Memperbesar ukuran logo di halaman login

--- src/App.tsx
+++ src/App.tsx
@@ -1131,7 +1131,7 @@ export default function App() {
             <div className="text-center mb-8">
               <div className="w-16 h-16 bg-blue-600 rounded-2xl flex items-center justify-center mx-auto mb-4 shadow-lg shadow-blue-500/20">
                 {/* KODE BARU (Menggunakan logo kustom favicon.png Anda): */}
-              <img src="/favicon.png" alt="Logo Wang Ajaib Halal" className="w-16 h-16 mx-auto mb-4 object-contain" />
+             <img src="/favicon.png" alt="Logo Wang Ajaib Halal" className="w-32 h-32 mx-auto mb-4 object-contain" />
 
               </div>
               <h2 className="text-2xl font-extrabold tracking-tight">WANG AJAIB</h2>
